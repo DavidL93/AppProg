0d2f8b8c8d48f70d0c1a7d8217d8298bddd7fdf7
DavidL93 <davidlay3093@gmail.com>
2018-03-14 16:21:28 +1100

    added module 3 and 5, finished java refresher

---
+++ Java Refresher/Module 3 - Defining classes (basic)/Module3/src/Exercise6.java
@@ -0,0 +1,8 @@
+public class Exercise6 {
+	public static void main(String[] args){
+                Rectangle.showNumberOfSides();
+                Rectangle rectObj = new Rectangle();
+                rectObj.show();
+                rectObj.showArea();
+	}
+}
