a5646d2078d89899be9ef3d5bfcbd59bbb1f3d42
DavidL93 <davidlay3093@gmail.com>
2018-03-14 15:58:33 +1100

    finised module 5 and java refresher

---
+++ Java Refresher/Module 5 - Class composition and iteration/Module5/src/LookupPhonebook.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author David
+ */
+public class LookupPhonebook {
+    public static void main(String[] args) {
+        Phonebook phonebook = new Phonebook("Sam Johnson");
+        phonebook.addContact(new Contact("Kelly Wong", "(02) 12345678"));
+        phonebook.addContact(new Contact("Richard Jackson", "(02) 87654321"));
+        phonebook.show();
+        
+        String name = Input.askString("Enter a contact name:");
+        Contact cont = phonebook.findContactByName(name);
+        if(cont != null){
+            System.out.println("Phone number is " + cont.getPhone());
+        } else{
+            System.out.println(name + " not found");
+        }
+    }
+}
---
+++ Java Refresher/Module 5 - Class composition and iteration/Module5/src/Phonebook.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author David
+ */
+import java.util.*;
+
+public class Phonebook {
+    private String owner; 
+    private ArrayList<Contact> contacts = new ArrayList<Contact>();
+    
+    public Phonebook(String owner){
+        this.owner = owner;
+    }
+    
+    public void addContact(Contact c){
+        contacts.add(c);
+    }
+    
+    public void show(){
+        System.out.println(this.owner + "'s phonebook");
+        for(Contact contact: contacts){
+            System.out.println(contact.getName() + ": " + contact.getPhone());
+        }
+    }
+    
+    public Contact findContactByName(String name){
+        for(Contact contact: contacts){
+            if(contact.getName().equals(name)){
+                return contact;
+            }
+        }
+        return null;
+    }
+    
+    
+}
